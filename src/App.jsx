d6343a3cda192ed2b2c2a3787394fdeddc884ab4
Graciela Zhang <irishaze736@gmail.com>
2025-12-19 14:57:06 +0800

    finished App Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import { useState } from "react";
 import "./App.css";
+import vectorLogo from "./Vector.png";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div class="container">
-        <img src="./Vector.png" />
-        <h1 class="item">Meovv</h1>
-        <h2 class="item">
-          Find Your
-          <br /> People Here
-          <br /> at Northwestern
-        </h2>
-        <button class="item account">Create Account</button>
-        <br />
-        <button class="item signin">SSO Sign In</button>
-        <h3 class="item">Trouble signing in?</h3>
-        <footer class="item">
-          By signing up, you agree to our <span>Terms</span>. Learn
-          <br /> how we use your data in our <span>Privacy Policy.</span>
-        </footer>
+      <div class="background">
+        <div class="container">
+          <img src={vectorLogo} alt="Vector Logo" />
+          <h2 class="item">
+            Find Your
+            <br /> People Here at
+            <br />
+            Northwestern
+          </h2>
+          <button class="item account">Create Account</button>
+          <button class="item signin">SSO Sign In</button>
+          <h3 class="item">
+            <span>Trouble signing in?</span>
+          </h3>
+          <footer class="item">
+            By signing up, you agree to our <span>Terms</span>. Learn
+            <br /> how we use your data in our <span>Privacy Policy.</span>
+          </footer>
+        </div>
       </div>
     </>
   );
 }
-
 export default App;
