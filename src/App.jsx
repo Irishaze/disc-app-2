61359aa380e582e771a705f63a38a31d4071e69f
Graciela Zhang <irishaze736@gmail.com>
2025-12-22 10:49:32 +0800

    add new pages last changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
-import meovvLogo from "./icon.png";
-import discover from "./assets/discover.png";
-import discover_2 from "./assets/discover_y.png";
-import add from "./assets/add.png";
-import add_2 from "./assets/add_2.png";
-import likes from "./assets/likes.png";
-import likes_2 from "./assets/likes_2.png";
-import messages from "./assets/messages.png";
-import messages_2 from "./assets/messages_2.png";
-import me from "./assets/me.png";
-import me_2 from "./assets/me_2.png";
-import lamp from "./assets/lamp.png";
 
 function Discover() {
   return <h1>Discover Page</h1>;
@@ -42,25 +30,25 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <div class="background">
-          <div class="container">
-            <img src={meovvLogo} alt="Meovv Logo" />
-            <h2 class="item">
+        <div className="background">
+          <div className="container">
+            <img src="./src/icon.png" alt="Meovv Logo" />
+            <h2 className="item">
               Find Your
               <br /> People Here at
               <br />
               Northwestern
             </h2>
-            <button class="item account" style={{ backgroundColor: color }}>
+            <button className="item account" style={{ backgroundColor: color }}>
               Create Account
             </button>
-            <button class="item signin" style={{ color: color }}>
+            <button className="item signin" style={{ color: color }}>
               SSO Sign In
             </button>
-            <h3 class="item" style={{ color: color }}>
+            <h3 className="item" style={{ color: color }}>
               <span>Trouble signing in?</span>
             </h3>
-            <footer class="item" style={{ color: color }}>
+            <footer className="item" style={{ color: color }}>
               By signing up, you agree to our <span>Terms</span>. Learn
               <br /> how we use your data in our <span>Privacy Policy.</span>
             </footer>
@@ -76,33 +64,55 @@ function App() {
 function NavBar() {
   return (
     <>
-      <nav class="nav">
+      <nav className="nav">
         <NavLink to="/discover">
           {({ isActive }) => (
-            <img src={isActive ? discover_2 : discover} alt="discover icon" />
+            <img
+              src={
+                isActive
+                  ? "./src/assets/discover_2.png"
+                  : "./src/assets/discover.png"
+              }
+              alt="discover icon"
+            />
           )}
         </NavLink>
         <NavLink to="/messages">
           {({ isActive }) => (
             <img
-              src={isActive ? messages_2 : messages}
+              src={
+                isActive
+                  ? "./src/assets/messages_2.png"
+                  : "./src/assets/messages.png"
+              }
               alt="messages icon"
             ></img>
           )}
         </NavLink>
         <NavLink to="/add">
           {({ isActive }) => (
-            <img src={isActive ? add_2 : add} alt="add icon"></img>
+            <img
+              src={isActive ? "./src/assets/add_2.png" : "./src/assets/add.png"}
+              alt="add icon"
+            ></img>
           )}
         </NavLink>
         <NavLink to="/likes">
           {({ isActive }) => (
-            <img src={isActive ? likes_2 : likes} alt="likes icon"></img>
+            <img
+              src={
+                isActive ? "./src/assets/likes_2.png" : "./src/assets/likes.png"
+              }
+              alt="likes icon"
+            ></img>
           )}
         </NavLink>
         <NavLink to="/me">
           {({ isActive }) => (
-            <img src={isActive ? me_2 : me} alt="home icon"></img>
+            <img
+              src={isActive ? "./src/assets/me_2.png" : "./src/assets/me.png"}
+              alt="home icon"
+            ></img>
           )}
         </NavLink>
 
@@ -120,13 +130,19 @@ function NavBar() {
 
 function Lamp({ setColor }) {
   const toggleColor = () => {
-    setColor((prev) => (prev === "#FFE893" ? "white" : "#FFE893"));
+    setColor((initialColor) =>
+      initialColor === "white" ? "#FFE893" : "white"
+    );
   };
 
   return (
     <>
       <p>click me!</p>
-      <img class="lamp" src={lamp} onClick={toggleColor}></img>
+      <img
+        className="lamp"
+        src="./src/assets/lamp.png"
+        onClick={toggleColor}
+      ></img>
     </>
   );
 }
