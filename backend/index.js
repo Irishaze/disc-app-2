bca748a122cfbb1006b7d239b7adb9d045982bbe
Graciela Zhang <irishaze736@gmail.com>
2026-01-01 23:28:33 +0800

    update port

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/users", router);
 
-const PORT = process.env.VITE_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
